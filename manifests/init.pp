f68320d241c11b46a5b3da3ca0f8bf0b4080ff29
Kris Buytaert <sdog@inuits.be>
2012-02-17 22:49:30 +0100

    Making puppet-lint happy

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,23 +11,23 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class elasticsearch ($version="0.17.6-1.el6"){
+class elasticsearch ($version='0.17.6-1.el6'){
 
-  # TODO Elastic search is not running  even with this in here.. 
-  # Add hasssatus .. 
-  service {"elasticsearch":
-    ensure    => "running",
+  # TODO Elastic search is not running  even with this in here..
+  # Add hasssatus ..
+  service {'elasticsearch':
+    ensure    => 'running',
     hasstatus => 'true',
     enable    => 'true';
 
 
 
   }
-  # Elastic search version needs to be identical to the one used for Logstash ! 
+  # Elastic search version needs to be identical to the one used for Logstash !
 
   package {
-    "elasticsearch":
-      ensure => "$version";
+    'elasticsearch':
+      ensure => "${version}";
     }
 
 
