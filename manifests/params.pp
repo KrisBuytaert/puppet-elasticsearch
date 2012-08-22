492e6bcbdb425e28b92345ac7e26967c0e85757d
Jan Vansteenkiste <jan@vstone.eu>
2012-08-22 08:12:17 +0200

    Fix 2sp soft tab

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,7 +11,9 @@
 # Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class elasticsearch::params ($version='0.17.6-1.el6',
-                             $es_clustername='elasticsearch')
-                             {
+class elasticsearch::params (
+  $version        = '0.17.6-1.el6',
+  $es_clustername = 'elasticsearch'
+) {
+
 }
